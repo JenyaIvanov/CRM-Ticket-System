f20ffb8be1be7a8415cd4625271b91dac1725569
ジャイ子 <jak008@gmail.com>
2024-08-12 19:52:14 +0300

    Article View & Article Creation Redesign - Redesigned & Stylized Article creation page. - Redesigned & Stylized Article view. - Redesigned & Stylized Article edit view.

--- frontend/src/components/ArticleDetails.tsx
+++ frontend/src/components/ArticleDetails.tsx
@@ -4,6 +4,25 @@ import apiConfig from "../api/apiConfig";
 import { DecodedToken } from "../interfaces/DecodedToken";
 import { Categories } from "../interfaces/Categories";
 import JWT from "expo-jwt";
+import {
+  MdCancel,
+  MdCreate,
+  MdDeleteForever,
+  MdFormatAlignLeft,
+  MdFormatBold,
+  MdFormatColorFill,
+  MdFormatColorText,
+  MdFormatItalic,
+  MdFormatListBulleted,
+  MdFormatListNumbered,
+  MdFormatQuote,
+  MdFormatSize,
+  MdFormatUnderlined,
+  MdOutlineKeyboardReturn,
+} from "react-icons/md";
+import { GrAttachment } from "react-icons/gr";
+import { FaRegSave } from "react-icons/fa";
+import { BiDislike, BiLike } from "react-icons/bi";
 
 const ArticleDetails: React.FC = () => {
   const { articleId } = useParams<{ articleId: string }>();
@@ -22,6 +41,7 @@ const ArticleDetails: React.FC = () => {
   const [categories, setCategories] = useState<Categories[]>([]);
   const [attachments, setAttachments] = useState<string[]>([]);
   const [createdBy, setCreatedBy] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
 
   useEffect(() => {
     // Retrieve JWT token from localStorage
@@ -89,6 +109,7 @@ const ArticleDetails: React.FC = () => {
         apiConfig.get(`/users/${article.author_id}`),
       ]);
       setCreatedBy(createdByResponse[0].data.username);
+      setProfilePicture(createdByResponse[0].data.profile_picture);
     };
 
     fetchArticleDetails();
@@ -132,28 +153,31 @@ const ArticleDetails: React.FC = () => {
     }
   };
 
-  const handleBackToArticles = () => {
-    navigate("/knowledgebase");
-  };
-
   const renderAttachments = () => {
     return attachments.map((attachment: string, index: number) => (
-      <a
-        key={index}
-        href={`http://localhost:3000/${attachment}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        File {index}
-      </a>
+      <div className="px-[0.5rem] py-[0.3rem] rounded-lg shadow text-lg font-poppins bg-gradient-to-br from-teal-400 to-emerald-500  text-white font-thin hover:scale-[104%] transition duration-150">
+        <a
+          className="flex flex-row items-center gap-2"
+          key={index}
+          href={`http://localhost:3000/${attachment}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GrAttachment />
+          Attachment
+        </a>
+      </div>
     ));
   };
 
   return (
-    <div>
-      {/* Above Article Display (This Section Always Shown) */}
-      <div>
-        <button onClick={handleBackToArticles}>Back to Articles</button>
+    <div className="flex flex-col ms-5">
+      <div className="mt-4 mb-6 text-white font-thin flex flex-row items-center gap-2 px-[0.7rem] py-[0.5rem] bg-gradient-to-br from-emerald-500 to-teal-400 rounded-md shadow w-fit hover:shadow-lg hover:scale-[104%] transition duration-100">
+        {/* Create A New Article */}
+        <MdOutlineKeyboardReturn className="text-xl" />
+        <button onClick={() => navigate("/knowledgebase")}>
+          Return To Knowledgebase
+        </button>
       </div>
 
       {/* Article */}
@@ -162,76 +186,180 @@ const ArticleDetails: React.FC = () => {
           {/* Article Edit Mode View */}
           {editMode &&
           (userRole === "admin" || userID === article.author_id) ? (
-            <div>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
-              <p>Category</p>
-              <select
-                id="category-select"
-                value={editCategoryId}
-                onChange={(e) => {
-                  setEditCategoryId(
-                    Number(e.target.options[e.target.selectedIndex].value)
-                  );
-                  setCategory(e.target.options[e.target.selectedIndex].text);
-                }}
-              >
-                <option value="" disabled>
-                  Select a category
-                </option>
-                {categories.map((cat) => (
-                  <option key={cat.id} value={cat.id}>
-                    {cat.title}
+            <div className="w-11/12 h-fit">
+              <h1 className="my-1 text-3xl font-poppins text-slate-600">
+                Edit Article
+              </h1>
+              <p className="mt-1 mb-4 font-thin text-slate-500">
+                Edit and update articles. Modify the title and content to ensure
+                the information remains accurate and up-to-date for other users.
+              </p>
+              <div className="my-3 flex flex-row gap-3 items-center">
+                <p className="font-poppins font-bold text-slate-600">
+                  Category
+                </p>
+                <select
+                  className="border p-1 border-emerald-400 shadow rounded-md w-fit focus:outline-emerald-400"
+                  id="category-select"
+                  value={editCategoryId}
+                  onChange={(e) => {
+                    setEditCategoryId(
+                      Number(e.target.options[e.target.selectedIndex].value)
+                    );
+                    setCategory(e.target.options[e.target.selectedIndex].text);
+                  }}
+                >
+                  <option value="" disabled>
+                    Select a category
                   </option>
-                ))}
-              </select>
-              <button onClick={handleEdit}>Save</button>
-              <button onClick={() => setEditMode(false)}>Cancel</button>
+                  {categories.map((cat) => (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex flex-col gap-1 mb-3">
+                <p className="font-bold font-poppins text-slate-600">Title</p>
+                <input
+                  className="border p-2 border-emerald-400 shadow rounded-lg w-full focus:outline-emerald-400"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="bg-gradient-to-br flex items-center flex-row from-teal-500 to-emerald-600 rounded-t-lg text-white p-2">
+                  <MdFormatBold className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatItalic className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatUnderlined className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatSize className="text-xl mx-1 hover:cursor-pointer" />
+
+                  <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                    |
+                  </p>
+                  <MdFormatColorFill className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatColorText className="text-xl mx-1 hover:cursor-pointer" />
+
+                  <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                    |
+                  </p>
+                  <MdFormatAlignLeft className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatListBulleted className="text-xl mx-1 hover:cursor-pointer" />
+                  <MdFormatListNumbered className="text-xl mx-1 hover:cursor-pointer" />
+
+                  <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                    |
+                  </p>
+                  <MdFormatQuote className="text-xl mx-1 hover:cursor-pointer" />
+                </div>
+                <textarea
+                  className="border p-2 min-h-[55vh] border-emerald-400 shadow rounded-b-lg w-full h-full focus:outline-emerald-400"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+              </div>
+
+              <div className="flex flex-row gap-2 mt-3">
+                <button
+                  className="py-[0.3rem] px-[1rem] text-lg flex flex-row gap-2 rounded-lg border shadow hover:scale-[104%] transition duration-150 bg-gradient-to-br from-cyan-400 to-emerald-500 text-white font-thin items-center"
+                  onClick={handleEdit}
+                >
+                  <FaRegSave />
+                  Save
+                </button>
+                <button
+                  className="py-[0.3rem] px-[0.8rem] text-lg flex flex-row gap-2 rounded-lg border shadow hover:scale-[104%] transition duration-150 bg-gradient-to-br from-cyan-400 to-slate-500 text-white font-thin items-center"
+                  onClick={() => setEditMode(false)}
+                >
+                  <MdCancel />
+                  Cancel
+                </button>
+              </div>
             </div>
           ) : (
-            <div>
+            <div className="w-11/12 h-full">
               {/* Main  Article View */}
-              <h1>{article.title}</h1>
+              <h1 className="text-4xl font-poppins w-full">{article.title}</h1>
+              <p className="mb-2 font-thin text-sm">Category: {category}</p>
 
               {/* Article: Details Section */}
-              <p>
-                Date Created: {new Date(article.date_created).toLocaleString()}
-              </p>
-              <p>Author: {createdBy}</p>
 
-              <p>Category: {category}</p>
+              <div className="flex flex-row items-center mb-5">
+                <div>
+                  <img
+                    className=" object-contain rounded-full shadow-sm"
+                    src={"http://localhost:3000/" + profilePicture}
+                    alt="Profile"
+                    width="55"
+                  />
+                </div>
+                <div className="flex flex-row justify-between items-center w-full">
+                  <div className="flex flex-col ms-2">
+                    <p className="font-poppins">{createdBy}</p>
+                    <p className="font-thin text-sm">
+                      {new Date(article.date_created).toLocaleString()}
+                    </p>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    {/* Article: Functions Section */}
+
+                    {/* Article: Edit */}
+                    {userRole === "admin" || userID === article.author_id ? (
+                      <button
+                        className="flex flex-row text-white text-lg items-center gap-1 px-[0.5rem] py-[0.3rem]  font-thin bg-gradient-to-br from-teal-400 to-emerald-500 rounded-xl shadow hover:scale-[104%] transition duration-150"
+                        onClick={() => setEditMode(true)}
+                      >
+                        <MdCreate className="text-lg" />
+                        Edit Article
+                      </button>
+                    ) : (
+                      ""
+                    )}
+
+                    {/* Article: Admin Functions */}
+                    {userRole === "admin" && (
+                      <div>
+                        <button
+                          className="flex flex-row text-lg text-white items-center gap-1 px-[0.5rem] py-[0.3rem]  font-thin bg-gradient-to-br from-cyan-500 to-cyan-700 rounded-xl shadow hover:scale-[104%] transition duration-150"
+                          onClick={handleDelete}
+                        >
+                          <MdDeleteForever className="text-lg" />
+                          Delete Article
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
 
               {/* Article: Body (Description) Section */}
-              <p>{article.text}</p>
+              <div className="w-full p-4 min-h-[40vh] h-full border rounded-lg bg-slate-50">
+                <p className="whitespace-pre-line">{article.text}</p>
+              </div>
 
               {/* Article: Attachments Section */}
-              <div>{renderAttachments()}</div>
+              <div className="flex flex-row gap-3 w-11/12 mt-2 mb-5">
+                {renderAttachments()}
+              </div>
 
-              <div>
-                {/* Article: Functions Section */}
-
-                {/* Article: Edit */}
-                {userRole === "admin" || userID === article.author_id ? (
-                  <button onClick={() => setEditMode(true)}>
-                    Edit Article
-                  </button>
-                ) : (
-                  ""
-                )}
-
-                {/* Article: Admin Functions */}
-                {userRole === "admin" && (
-                  <div>
-                    <button onClick={handleDelete}>Delete Article</button>
+              {/* Article: Feedback (NOT IMPLEMENTED)*/}
+              <div className="flex flex-col mb-5">
+                <div>
+                  <p className="text-lg font-thin mb-1">Was this helpful?</p>
+                </div>
+                <div className="flex flex-row items-center gap-3">
+                  <div className="flex flex-row items-center gap-1 text-lg px-[0.4rem] py-[0.3rem] border shadow rounded-md hover:cursor-pointer hover:scale-105 transition duration-150">
+                    <BiLike />
+                    Yes
                   </div>
-                )}
+
+                  <div className="flex flex-row items-center gap-1 text-lg px-[0.4rem] py-[0.3rem] border shadow rounded-md hover:cursor-pointer hover:scale-105 transition duration-150">
+                    <BiDislike />
+                    No
+                  </div>
+                </div>
               </div>
             </div>
           )}
--- frontend/src/components/CreateArticle.tsx
+++ frontend/src/components/CreateArticle.tsx
@@ -4,6 +4,20 @@ import apiConfig from "../api/apiConfig";
 import { DecodedToken } from "../interfaces/DecodedToken";
 import { Categories } from "../interfaces/Categories";
 import JWT from "expo-jwt";
+import {
+  MdFormatAlignLeft,
+  MdFormatBold,
+  MdFormatColorFill,
+  MdFormatColorText,
+  MdFormatItalic,
+  MdFormatListBulleted,
+  MdFormatListNumbered,
+  MdFormatQuote,
+  MdFormatSize,
+  MdFormatUnderlined,
+  MdOutlineKeyboardReturn,
+} from "react-icons/md";
+import { FaPlusCircle } from "react-icons/fa";
 
 const CreateArticle: React.FC = () => {
   const [user_id, setUserId] = useState(0);
@@ -84,24 +98,27 @@ const CreateArticle: React.FC = () => {
   };
 
   return (
-    <div>
-      <button onClick={() => navigate("/knowledgebase")}>
-        Return To Knowledgebase
-      </button>
-      <h1>Create New Article</h1>
+    <div className="flex flex-col ms-5 w-11/12">
+      <div className="mt-3 mb-4 text-white font-thin flex flex-row items-center gap-2 px-[0.7rem] py-[0.5rem] bg-gradient-to-br from-emerald-500 to-teal-400 rounded-md shadow w-fit hover:shadow-lg hover:scale-[104%] transition duration-100">
+        {/* Create A New Article */}
+        <MdOutlineKeyboardReturn className="text-xl" />
+        <button onClick={() => navigate("/knowledgebase")}>
+          Return To Knowledgebase
+        </button>
+      </div>
+      <h1 className="my-1 text-3xl font-poppins text-slate-600">
+        Create New Article
+      </h1>
+      <p className="mt-1 mb-4 font-thin text-slate-500">
+        Create and publish articles for other users. Provide a title, write the
+        content, and attach any necessary files to share detailed instructions
+        or information.
+      </p>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <p>Category: </p>
+        <div className="flex flex-row gap-3 items-center">
+          <p className="font-poppins font-bold text-slate-600">Category</p>
           <select
+            className="border p-1 border-emerald-400 shadow rounded-md w-fit focus:outline-emerald-400"
             id="category-select"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
@@ -116,19 +133,65 @@ const CreateArticle: React.FC = () => {
             ))}
           </select>
         </div>
-        <div>
-          <label>Text:</label>
-          <textarea
-            value={text}
-            onChange={(e) => setText(e.target.value)}
+
+        <div className="flex flex-col gap-1 mb-3">
+          <p className="font-bold font-poppins text-slate-600">Title</p>
+          <input
+            className="border p-2 border-emerald-400 shadow rounded-lg w-full focus:outline-emerald-400"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label>Attachments:</label>
+
+        <div className="flex flex-row items-center gap-3 mb-3">
+          <div className="flex flex-col w-full">
+            <div className="bg-gradient-to-br flex items-center flex-row from-teal-500 to-emerald-600 rounded-t-lg text-white p-2">
+              <MdFormatBold className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatItalic className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatUnderlined className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatSize className="text-xl mx-1 hover:cursor-pointer" />
+
+              <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                |
+              </p>
+              <MdFormatColorFill className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatColorText className="text-xl mx-1 hover:cursor-pointer" />
+
+              <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                |
+              </p>
+              <MdFormatAlignLeft className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatListBulleted className="text-xl mx-1 hover:cursor-pointer" />
+              <MdFormatListNumbered className="text-xl mx-1 hover:cursor-pointer" />
+
+              <p className="mx-2 opacity-90 font-thin mt-[-0.25rem] hover:cursor-default">
+                |
+              </p>
+              <MdFormatQuote className="text-xl mx-1 hover:cursor-pointer" />
+            </div>
+            <textarea
+              className="border p-2 min-h-[50vh] border-emerald-400 shadow rounded-b-lg w-full h-full focus:outline-emerald-400"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-3 mb-3">
+          <p className="font-bold font-poppins text-slate-600 my-2">
+            Attachments
+          </p>
           <input type="file" multiple onChange={handleFileChange} />
         </div>
-        <button type="submit">Create Article</button>
+        <button
+          className="rounded-lg mb-2 shadow px-[0.4rem] py-[0.4rem] bg-gradient-to-br from-teal-400 to-emerald-500 text-white font-thin hover:scale-[104%] transition duration-150 flex flex-row items-center gap-2 text-lg"
+          type="submit"
+        >
+          <FaPlusCircle className="text-xl" />
+          Publish Article
+        </button>
       </form>
     </div>
   );
